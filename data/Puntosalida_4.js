0bafe897ba20f6041fbf03ae96c39678037ee45e
Javier-Est15 <86083164+Javier-Est15@users.noreply.github.com>
2021-06-25 21:24:35 -0500

    Add files via upload

---
+++ data/Puntosalida_4.js
@@ -0,0 +1,1 @@
+var json_Puntosalida_4 = {"type":"FeatureCollection","name":"Puntosalida_4","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[{"type":"Feature","properties":{"fid":"1","Nombre":"Punto Salida"},"geometry":{"type":"Point","coordinates":[-110.193355524305687,23.356775216704246]}}]}
